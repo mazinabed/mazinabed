cf2f4cd1be973893b446aeaa369c607299e1c386
Your Name <you@example.com>
2020-04-22 17:48:38 -0500

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,152 @@
+let resumeData = {
+    "imagebaseurl":'/images/mazin.png',
+    "name": "Mazin Abed",
+    "role": "Full stack Developer with 10 years of Logistics experience",
+    "linkedinId":"",
+    //"skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of development and like to learn about new technologies, practice and read in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mazin-abed-90485826/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mazinabed",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/mazin_a_abd",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Full Stack Web Developer with a demonstrated history of working in Logistics and warehouse industry. 10 years of experience in the transportation, trucking and warehouse management  systems. Skilled in Customer Service, Microsoft Office, SAP, Oracle, frontend-backend web developer. Strong leadership skills. ",
+    "address":"Austin,TX",
+    //"website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Wasit university in Iraq, College of Education",
+        "specialization":"English literature",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2008",
+        "Achievements":"BA in English literature"
+      },
+      {
+        "UniversityName":"The University of Texas at Austin, Bootcamp",
+        "specialization":"Wep Development",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2020",
+        "Achievements":"Full stack developer"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Ryder.inc",
+        "specialization":"CLS Customer Logistics supervisor",
+        "MonthOfLeaving":"11/01/2017",
+        "YearOfLeaving":"04/30/2019",
+        "Achievements":"CLS is a dedicated to an assigned area of responsibility within an account or location. The incumbent ensures that productivity and operational goals are met in order to deliver customer satisfaction. Individual will be working in a dynamic environment that focuses on delivering continuous measurable improvement to the customer."
+      },
+      {
+        "CompanyName":"Ryder.inc",
+        "specialization":"Weekend super lead",
+        "MonthOfLeaving":"04/01/2017",
+        "YearOfLeaving":"10/31/2017",
+        "Achievements":"The weekend Lead is to oversee the daily operation of assigned warehouse function/de- partment(s) to ensure compliance with Ryder policies and procedures through safely verifying product quantity, quality, and efficiency."
+      },
+      {
+        "CompanyName":"Ryder.inc",
+        "specialization":"Shipping & Large parts Lead",
+        "MonthOfLeaving":"04/01/2013",
+        "YearOfLeaving":"03/31/2017",
+        "Achievements":"This position is responsible for insuring all day-to-day activities within assigned section are successfully accomplished. Insures all processes are followed and is responsible for pro- viding proficiency training when necessary. In addition, Responsible for issuing adherence to all Ryder and work center Safety and Security policies."
+      },
+      {
+        "CompanyName":"Ryder.inc",
+        "specialization":"Materials handler",
+        "MonthOfLeaving":"04/01/2010",
+        "YearOfLeaving":"03/31/2013",
+        "Achievements":"I start working with Ryder as a temporary worker report to Randstad Company. Then, in May, 2012 I got hired as a full time employee with Ryder.During this period, I worked in NSSC where I moved to different area such as; production line, Audit, Back order, kitting, large parts, shipping, large part Audit and QA.I deal with many work applications on daily basis, including but not limited to: Oracle, SRT, Kit audit, SFC, LTS, TW and Shipment Tracker and other applications related to shipping and large part processes."
+      },
+      {
+        "CompanyName":"INTERPRETER, TITANIC, L3 COMMUNICATION AND GLS",
+        "specialization":"Translation and cultural advisor",
+        "MonthOfLeaving":"03/01/2003",
+        "YearOfLeaving":"08/31/2008",
+        "Achievements":"Prepare nontechnical translations into the target language and perform sight translations from a target language into English. Provide interpretation support at military traffic control points and local media events. Also, Provide support for but not limited to:Checkpoint Operations, Medical Support, Locally Employed Personnel Screening, Training Host-Nation Forces, VIP Escort and Contract Negotiations and Cultural Awareness."
+      }
+    ],
+    "skillsDescription":"Wep development skills",
+    
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"GitHub"
+      },
+      
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"javaScript"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"jquery"
+      },
+      {
+        "skillname":"NEXTjs"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Food and Drink",
+        "link":"https://mazinabed.github.io/projectOne/",
+        "description":"In this project user can search for food or drink by name and a list of options will pops up. The list will incloud Picture of the meal, recipes, ingredient and a video. Also, there is a Random Button that will generate a random meal with drink.",
+        "imgurl":"images/portfolio/FoodDrink.png"
+      },
+      {
+        "name":"Search the Scientific",
+        "link":"https://infinite-stream-60071.herokuapp.com/login",
+        "description":"This app offers the user a method of searching for scientific and mathematical information using the Wolfram Alpha API.",
+        "imgurl":"images/portfolio/SearchScientific.png"
+      },
+      {
+        "name":"Book React App",
+        "link":"https://secret-stream-06395.herokuapp.com",
+        "description":"React-based Google Books Search app. This SPA (Single Page Application) uses [react-router-dom]to navigate, search for book and save books",  
+        "imgurl":"images/portfolio/BookSearch.png"
+      },
+      {
+        "name":"Fitness Tracker",
+        "link":"https://morning-garden-08276.herokuapp.com/?id=5e9f847e48b4460017ffad0c",
+        "description":"This app allows users to track and record their daily activity.",
+        "imgurl":"images/portfolio/FitnessTracker.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ],
+    "projects":[
+
+    ]
+
+  }
+  
+  export default resumeData
